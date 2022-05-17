74f44805a66211084b90046d3b7ab64e19b95edf
ijanus <emekamicro@gmail.com>
2022-05-17 12:04:02 +0100

    WIP

--- GTK/Properties/AssemblyInfo.cs
+++ GTK/Properties/AssemblyInfo.cs
@@ -1,7 +1,3 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("Gtk")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyTrademark("")]
